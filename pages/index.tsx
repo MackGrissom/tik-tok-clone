dd05f803690c6a6f8166fe1f64426fd2d4b6e9fa
Mackadelicc <64600170+Mackadelicc@users.noreply.github.com>
2022-10-05 15:44:20 +0700

    added types and env file for api key...also added syntax for API call to retrieve videos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from 'next'
 import axios from 'axios'
-import { GiAxeInLog } from 'react-icons/gi'
 
-const Home: NextPage = () => {
+// interface IProps {
+//   videos:
+// }
+
+const Home: NextPage = ({ videos }) => {
   return (
     <div className='text-3xl font-bold underline'>
       VIVID
@@ -11,11 +14,13 @@ const Home: NextPage = () => {
 }
 
 export const getServerSideProps = async () => {
-  const response = await axios.get('http://localhost:3000/api/post');
+  const { data }  = await axios.get('http://localhost:3000/api/post');
 
-  console.log(response.data.name);
+  
   return {
-    props:{}
+    props: {
+      videos: data,
+    }
   }
 }
 export default Home
